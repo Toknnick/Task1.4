d09e48767ce009ceecf6e99792d5835171910ddb
Maksim <maksimus.tumannick@yandex.ru>
2022-06-20 20:12:48 +0300

    Task6

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 52;
+            int row = 0;
+            
+            for (;total > 3; row++)
+            {
+                total -= 3;
+            }
+            Console.WriteLine($"Заолняно рядов : {row}");
+            Console.WriteLine($"Осталось картинок : {total}");
+        }
+    }
+}
